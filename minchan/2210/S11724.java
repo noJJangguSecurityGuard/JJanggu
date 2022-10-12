c5f4982f9f84d5c586197247def84b62089aafd7
김민찬 <81581362+nsmchan@users.noreply.github.com>
2022-10-12 21:12:10 +0900

    연결리스트 문제 주석작성 11724

--- minchan/2210/S11724.java
+++ minchan/2210/S11724.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 public class S11724 { 
 
+    // static 영역에 저장함으로 전역변수처럼 사용
     static ArrayList<Integer>[] list;
     static boolean[] visited;
     public static void main(String[] args) throws IOException{
@@ -18,9 +19,9 @@ public class S11724 {
         int edge = Integer.parseInt(st.nextToken());
         //int start = Integer.parseInt(st.nextToken());
 
-        list = new ArrayList[node+1];
+        list = new ArrayList[node+1];   
         visited = new boolean[node+1]; // 초기값은 false
-        
+
     
         
         // 인접리스트 초기화  노드수 만큼, 1부터시작
@@ -41,7 +42,7 @@ public class S11724 {
         // dfs 호출
         int cnt =0;
         for(int i=1;i<node+1 ;i++){
-            if(!visited[i]){
+            if(!visited[i]){    // 방문하지않았으면
                 cnt++;
                 DFS(i);
             }
